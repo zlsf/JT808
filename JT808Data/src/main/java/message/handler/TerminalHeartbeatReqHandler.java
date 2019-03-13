18ea1aebb798f4eeca79359949ad9b016feb55c9
Administrator <Administrator@PC201607051436>
2019-03-13 17:38:54 +0800

    心跳

---
+++ JT808Data/src/main/java/message/handler/TerminalHeartbeatReqHandler.java
@@ -0,0 +1,19 @@
+
+package message.handler;
+
+import Utils.JT808Constant;
+import message.InboundMessageHandler;
+import model.Session;
+import model.codec.JT808Packet;
+
+/**
+ * 心跳包处理
+ *
+ */
+public class TerminalHeartbeatReqHandler implements InboundMessageHandler {
+
+	public void handle(Session session, JT808Packet packetData) {
+		session.sendPlatformGeneralRsp(packetData.getTerminalId(), packetData.getMsgNo(), packetData.getMsgId(),
+				JT808Constant.GENERAL_RSP_RESULT_SUCCESS);
+	}
+}
