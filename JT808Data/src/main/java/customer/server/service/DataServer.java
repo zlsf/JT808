4e4cfe7f5977378335471d3d42a772ad084f8ff4
Administrator <Administrator@PC201607051436>
2019-06-21 08:22:51 +0800

    格式化

--- JT808Data/src/main/java/customer/server/service/DataServer.java
+++ JT808Data/src/main/java/customer/server/service/DataServer.java
@@ -21,90 +21,90 @@ import io.netty.handler.timeout.IdleStateHandler;
 
 public final class DataServer implements Runnable {
 
-    private Logger log = LoggerFactory.getLogger(DataServer.class);
+	private Logger log = LoggerFactory.getLogger(DataServer.class);
 
-    private ExecutorService serverExecutor = Executors.newSingleThreadExecutor();
+	private ExecutorService serverExecutor = Executors.newSingleThreadExecutor();
 
-    /** The future. */
-    private Future<?> future;
+	/** The future. */
+	private Future<?> future;
 
-    private int port = 8899;
+	private int port = 8899;
 
-    private volatile boolean running = false;
+	private volatile boolean running = false;
 
-    private DataServiceHandler dataServiceHandler;
+	private DataServiceHandler dataServiceHandler;
 
-    private static Object locker = new Object();
+	private static Object locker = new Object();
 
-    /**
-     * Sets the port.
-     *
-     * @param port
-     *            the new port
-     */
-    public void setPort(int port) {
-	this.port = port;
-    }
-
-    public DataServer() {
-	dataServiceHandler = new DataServiceHandler();
-    }
-
-    public void start() {
-	log.info("开启服务器......");
-	synchronized (locker) {
-	    this.running = true;
-	    future = serverExecutor.submit(this);
+	/**
+	 * Sets the port.
+	 *
+	 * @param port
+	 *            the new port
+	 */
+	public void setPort(int port) {
+		this.port = port;
 	}
-	log.info("开启服务结束......");
-    }
 
-    /**
-     * 停止锟斤拷锟斤拷.
-     */
-    public void stop() {
-	log.info("停止服务......");
-	synchronized (locker) {
-	    if (!running)
-		return;
-
-	    if (future == null)
-		return;
-
-	    future.cancel(running);
-	    future = null;
-	    this.running = false;
+	public DataServer() {
+		dataServiceHandler = new DataServiceHandler();
 	}
-	log.info("停止服务结束......");
-    }
 
-    @Override
-    public void run() {
-	EventLoopGroup bossGroup = new NioEventLoopGroup();
-	EventLoopGroup workerGroup = new NioEventLoopGroup();
-	try {
-	    ServerBootstrap b = new ServerBootstrap();
-	    b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).option(ChannelOption.SO_BACKLOG, 128)
-		    .option(ChannelOption.TCP_NODELAY, true).childOption(ChannelOption.SO_KEEPALIVE, true)
-		    .childOption(ChannelOption.TCP_NODELAY, true).childHandler(channelnit);
-
-	    b.bind(port).sync().channel().closeFuture().sync();
-	} catch (Exception e) {
-	    running = false;
-	    log.warn("开启Netty服务异常", e);
-	} finally {
-	    bossGroup.shutdownGracefully();
-	    workerGroup.shutdownGracefully();
+	public void start() {
+		log.info("开启服务器......");
+		synchronized (locker) {
+			this.running = true;
+			future = serverExecutor.submit(this);
+		}
+		log.info("开启服务结束......");
 	}
-    }
 
-    private ChannelInitializer<SocketChannel> channelnit = new ChannelInitializer<SocketChannel>() {
+	/**
+	 * 停止锟斤拷锟斤拷.
+	 */
+	public void stop() {
+		log.info("停止服务......");
+		synchronized (locker) {
+			if (!running)
+				return;
+
+			if (future == null)
+				return;
+
+			future.cancel(running);
+			future = null;
+			this.running = false;
+		}
+		log.info("停止服务结束......");
+	}
 
 	@Override
-	protected void initChannel(SocketChannel channel) throws Exception {
-	    channel.pipeline().addLast("logging", new LoggingHandler(LogLevel.TRACE))
-		    .addLast("idle-handler", new IdleStateHandler(100, 100, 100))
-		    .addLast("service-handler", dataServiceHandler);
+	public void run() {
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).option(ChannelOption.SO_BACKLOG, 128)
+					.option(ChannelOption.TCP_NODELAY, true).childOption(ChannelOption.SO_KEEPALIVE, true)
+					.childOption(ChannelOption.TCP_NODELAY, true).childHandler(channelnit);
+
+			b.bind(port).sync().channel().closeFuture().sync();
+		} catch (Exception e) {
+			running = false;
+			log.warn("开启Netty服务异常", e);
+		} finally {
+			bossGroup.shutdownGracefully();
+			workerGroup.shutdownGracefully();
+		}
 	}
-    };
+
+	private ChannelInitializer<SocketChannel> channelnit = new ChannelInitializer<SocketChannel>() {
+
+		@Override
+		protected void initChannel(SocketChannel channel) throws Exception {
+			channel.pipeline().addLast("logging", new LoggingHandler(LogLevel.TRACE))
+					.addLast("idle-handler", new IdleStateHandler(100, 100, 100))
+					.addLast("service-handler", dataServiceHandler);
+		}
+	};
 }
