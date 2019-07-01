4aa6c09185756b56c97bf25fe19b8b6518d310bd
zlsf <zlsf@zlsf.lan>
2019-07-01 22:05:15 +0800

    多设备下发

--- JT808Data/src/main/java/customer/server/process/MessageSender.java
+++ JT808Data/src/main/java/customer/server/process/MessageSender.java
@@ -2,6 +2,8 @@
 package customer.server.process;
 
 import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import com.alibaba.fastjson.JSON;
 import com.kj.datacenter.msg.GeneralRspMsg;
 
+import customer.server.model.Session;
+import customer.server.model.SessionManager;
 import customer.server.process.msg.AbstractMessage;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
@@ -30,8 +34,39 @@ public class MessageSender {
 		private static final MessageSender INSTANCE = new MessageSender();
 	}
 
+	public static void sendToDevice(List<String> deviceIDs, byte[] data) {
+		if (deviceIDs == null || deviceIDs.size() <= 0)
+			throw new RuntimeException("请传入设备号");
+		for (String deviceId : deviceIDs) {
+			try {
+				Session session = SessionManager.getInstance().findByDeviceId(deviceId);
+				if (session == null)
+					continue;
+				sendToDevice(session.getChannel(), data);
+			} catch (Exception ex) {
+				log.error("设备号：" + deviceId + "发送数据失败。");
+				log.error(ex.getMessage());
+			}
+		}
+	}
+
 	/**
-	 * 锟斤拷锟酵碉拷锟借备
+	 * 下发到所有设备
+	 * @param data
+	 */
+	public static void sendToAllDevice(byte[] data) {
+		Map<String, Session> sessionmap = SessionManager.getInstance().getSessionMap();
+		for (String key : sessionmap.keySet()) {
+			try {
+				sendToDevice(sessionmap.get(key).getChannel(), data);
+			} catch (Exception ex) {
+				log.error(ex.getMessage());
+			}
+		}
+	}
+
+	/**
+	 * 下发到设备
 	 * 
 	 * @param channel
 	 * @param data
