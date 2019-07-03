6c96b391853d1d42beee0578bfdc30f103d728ea
zlsf <zlsf@zlsf.lan>
2019-07-03 20:58:47 +0800

    添加處理類

--- JT808Data/src/main/java/customer/server/process/msg/MessageFactory.java
+++ JT808Data/src/main/java/customer/server/process/msg/MessageFactory.java
@@ -20,21 +20,21 @@ import customer.server.process.msg.detail.TestHandel;
  */
 public class MessageFactory {
 
-	private final static Object locker = new Object();
+    private final static Object locker = new Object();
 
-	private MessageFactory() {
-	}
+    private MessageFactory() {
+    }
 
-	public static MessageFactory getInstance() {
-		return MessageFactoryInstance.INSTANCE;
-	}
+    public static MessageFactory getInstance() {
+	return MessageFactoryInstance.INSTANCE;
+    }
 
-	private static class MessageFactoryInstance {
+    private static class MessageFactoryInstance {
 
-		private static final MessageFactory INSTANCE = new MessageFactory();
-	}
+	private static final MessageFactory INSTANCE = new MessageFactory();
+    }
 
-	/**
+    /**
 	 * 锟斤拷品锟斤拷
 	 */
 	private Map<Integer, Class<? extends AbstractMessage>> dic = new HashMap<Integer, Class<? extends AbstractMessage>>() {
@@ -46,28 +46,37 @@ public class MessageFactory {
 		}
 	};
 
-	/**
-	 * Gets the cmd.
-	 *
-	 * @param <T>
-	 *            the generic type
-	 * @param msgFlag
-	 *            the msg flag
-	 * @param args
-	 *            the args
-	 * @return the cmd
-	 */
-	public <T extends AbstractMessage> T getCmd(Integer msgFlag, Object... args) {
-		if (!dic.containsKey(msgFlag))
-			return null;
+    /**
+     * 注册数据处理类
+     * @param messageId
+     * @param clazz
+     */
+    public void regestHandler(int messageId, Class<? extends AbstractMessage> clazz) {
+	this.dic.put(messageId, clazz);
+    }
 
-		try {
-			Constructor cs = dic.get(msgFlag).getConstructor(PackData.class);
-			cs.setAccessible(true);
-			T result = (T) cs.newInstance(args);
-			return result;
-		} catch (Exception e) {
-			throw new RuntimeException();
-		}
+    /**
+     * Gets the cmd.
+     *
+     * @param <T>
+     *            the generic type
+     * @param msgFlag
+     *            the msg flag
+     * @param args
+     *            the args
+     * @return the cmd
+     */
+    public <T extends AbstractMessage> T getCmd(Integer msgFlag, Object... args) {
+	if (!dic.containsKey(msgFlag))
+	    return null;
+
+	try {
+	    Constructor cs = dic.get(msgFlag).getConstructor(PackData.class);
+	    cs.setAccessible(true);
+	    T result = (T) cs.newInstance(args);
+	    return result;
+	} catch (Exception e) {
+	    throw new RuntimeException();
 	}
+    }
 }
